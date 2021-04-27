6380aafcb8d0f5f478387ee59d6ead044e3aadd1
xuantruongcox <truongcool999@gmail.com>
2021-04-27 21:35:45 +0700

    fixed

--- src/app/dom-data/mock-user.ts
+++ src/app/dom-data/mock-user.ts
@@ -2,31 +2,10 @@ import {STUDENT} from './user';
 export const STUDENTS: STUDENT[] = [
     {
         id: 1,
-        name: "Nguyen Xuan Truong",
+        name: 'Nguyen Xuan Truong',
         dob: new Date('1998-12-1'),
         address: '',
         pNumber: 3213321,
-        role: "Student"
-    }, {
-        id: 1,
-        name: "Tran Van A",
-        dob: new Date('1998-12-1'),
-        address: '',
-        pNumber: 3213321,
-        role: "Student"
-    }, {
-        id: 1,
-        name: "Le Van B",
-        dob: new Date('1998-12-1'),
-        address: '',
-        pNumber: 3213321,
-        role: "Student"
-    }, {
-        id: 1,
-        name: "Trinh Phat C",
-        dob: new Date('1998-12-1'),
-        address: '',
-        pNumber: 3213321,
-        role: "Teacher"
-    }
+        role: 'Student'
+    },
 ]
--- src/app/dom-data/user.ts
+++ src/app/dom-data/user.ts
@@ -1,8 +1,8 @@
-export interface STUDENT{
-    id: number,
-    name: String,
-    dob: Date,
-    address: String,
-    pNumber: Number,
-    role: String
+export interface STUDENT {
+  id: number;
+  name: String;
+  dob: Date;
+  address: String;
+  pNumber: Number;
+  role: String;
 }
